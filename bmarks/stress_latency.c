f238f8a6e23166e1c00dceb9257e5e3ffc2e5d08
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2013-11-01 16:28:32 +0100

    fix: the stress_latency test was always writing to the protected cache lines on 1 of the 2 steps of the execution

--- bmarks/stress_latency.c
+++ bmarks/stress_latency.c
@@ -155,7 +155,11 @@ void *test(void *data)
             uint32_t i;
             for (i = 0; i < cl_access; i++)
             {
-                protected_data[i + lock_to_acq * cl_access].the_data[0]= d->id;
+	      if (do_writes==1) {
+                protected_data[i + protected_offsets[lock_to_acq]].the_data[0]+=d->id;
+	      } else {
+                protected_data[i + protected_offsets[lock_to_acq]].the_data[0]= d->id;
+	      }
             }
             release_lock(&local_d[lock_to_acq],&the_locks[lock_to_acq]);
             if (acq_delay>0) cpause(acq_delay);
