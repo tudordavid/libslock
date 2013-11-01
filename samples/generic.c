8f2eb670b2e2b7d36d6583512cce4badd273470a
Tudor David <tudor.david@epfl.ch>
2013-11-01 14:43:42 +0100

    added a sample working with a generic lock

--- samples/generic.c
+++ samples/generic.c
@@ -3,43 +3,56 @@
 #include <stdlib.h>
 #define NUM_THREADS 4
 
+#include "utils.h"
+#include "lock_if.h"
+
 
 /* global data */
 lock_global_data the_lock;
 
 void *do_something(void *id)
 {
-   int* my_core = (int*) id; 
-   lock_local_data my_data;
-   init_lock_local(*my_core, &the_lock, &my_data);
-
+    int* my_core = (int*) id; 
+    /* local data */
+    lock_local_data my_data;
+    /*initialize this thread's local data*/
+    init_lock_local(*my_core, &the_lock, &my_data);
     MEM_BARRIER;
+
+
+    /*acquire the lock*/
     acquire_lock(&my_data,&the_lock);
     printf("I have the lock\n");
-    release_lock(0,&my_data,&lock_id);
+    /*release the lock*/
+    release_lock(&my_data,&the_lock);
 
-    free_lock_local(&my_data);
+
+    MEM_BARRIER;
+    /*free internal memory structures which may have been allocated for the local data*/
+    free_lock_local(my_data);
+
+    return NULL;
 
 }
 
 int main(int argc, char *argv[])
 {
-   pthread_t threads[NUM_THREADS];
-   long t;
+    pthread_t threads[NUM_THREADS];
+    long t;
 
-   /*initialize the global data*/
-   init_lock_global(&the_lock); 
+    /*initialize the global data*/
+    init_lock_global(&the_lock); 
     int ids[]={0,1,2,3};
 
     MEM_BARRIER;
 
-   for(t=0;t<NUM_THREADS;t++){
-     printf("In main: creating thread %ld\n", t);
-     if (pthread_create(&threads[t], NULL, *do_something, &ids[t])!=0){
-        fprintf(stderr,"Error creating thread\n");
-        exit(-1);
-       }
-     }
+    for(t=0;t<NUM_THREADS;t++){
+        printf("In main: creating thread %ld\n", t);
+        if (pthread_create(&threads[t], NULL, *do_something, &ids[t])!=0){
+            fprintf(stderr,"Error creating thread\n");
+            exit(-1);
+        }
+    }
 
     for (t = 0; t < NUM_THREADS; t++) {
         if (pthread_join(threads[t], NULL) != 0) {
@@ -51,5 +64,5 @@ int main(int argc, char *argv[])
     /*free internal memory strucutres which may have been allocated for this lock */
     free_lock_global(the_lock);
 
-   pthread_exit(NULL);
+    pthread_exit(NULL);
 }
